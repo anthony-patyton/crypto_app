b4a64cb5daf0b95dc385226c5d9cc064d3c0d98f
Anthony Payton <84427236+anthony-patyton@users.noreply.github.com>
2021-09-28 18:42:01 -0600

    finished all the components ready for production

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import millify from 'millify';
 import HTMLReactParser from 'html-react-parser';
-import { Collapse, Row, Typography, Avatar } from 'antd';
+import { Collapse, Row, Col, Typography, Avatar } from 'antd';
+import Loader from './Loader';
 
 import { useGetExchangesQuery } from '../services/cryptoApi';
 
@@ -10,10 +11,46 @@ const { Panel } = Collapse;
 
 
 const Exchanges = () => {
+  const { data, isFetching } = useGetExchangesQuery('');
+  const exchangesList = data?.data?.exchanges;
+
+  if(isFetching) return <Loader />;
+  
   return (
-    <div>
-      Exchanges
-    </div>
+    <>
+      <Row>
+        <Col span={6}>Exchanges</Col> 
+        <Col span={6}>24H Trade Volume</Col> 
+        <Col span={6}>Markets</Col> 
+        <Col span={6}>Change</Col> 
+      </Row>
+      <Row>
+        {exchangesList.map((exchange) => (
+          <Col span={24}>
+            <Collapse>
+              <Panel
+                key={exchange.id}
+                showArrow={false}
+                header={(
+                  <Row key={exchange.id}>
+                    <Col span={6}>
+                      <Text><strong>{exchange.rank}.</strong></Text> 
+                      <Avatar src={exchange.iconUrl} alt="" className="exchange-image"/>
+                      <Text><strong>{exchange.name}</strong></Text> 
+                    </Col>
+                    <Col span={6}>{millify(exchange.volume)}</Col>
+                    <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
+                    <Col span={6}>{millify(exchange.marketShare)}%</Col>
+                  </Row>
+                )}
+              >
+                {HTMLReactParser(exchange.description || 'No Description' )}
+              </Panel>
+            </Collapse>
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
 
